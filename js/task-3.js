80301a44fa80ddca2755560f33cde9691bf87e5b
Viktoria <vvika8440@gmail.com>
2025-10-28 19:55:54 +0100

    hw-done

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,10 @@
 function checkForSpam(message) {
-    
+ const lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(checkForSpam("Latest technology news")); // false
--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,25 @@
+
 function getShippingCost(country) {
-    
+    let message; 
+
+    switch (country) {
+        case "Australia":
+            message = "Shipping to Australia will cost 170 credits";
+            break;
+        case "China":
+            message = "Shipping to China will cost 100 credits";
+            break;
+        case "Chile":
+            message = "Shipping to Chile will cost 250 credits";
+            break;
+        case "Jamaica":
+            message = "Shipping to Jamaica will cost 120 credits";
+            break;
+        default:
+            message = "Sorry, there is no delivery to your country";
+            break;
+    }
+  return message; 
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
